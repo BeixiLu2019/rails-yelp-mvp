fa9ca716f55247b535960bb83bd3ff8626d88367
Beixi Lu <beixi_lu@yahoo.com>
2020-05-18 19:09:16 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # namespace :admin do
-    resources :restaurants do
-      resources :reviews, only: [:new, :create]
-  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,13 +5,3 @@ require 'faker'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-5.times do
-  restaurant = Restaurant.create(
-    name:    Faker::Company.name,
-    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
-    phone_number: Faker::PhoneNumber.cell_phone,
-    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
-  )
-end
-
-
